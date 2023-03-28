9119b8a636386b1409dcb04c84c0a266a4ffe8ec
Michele Edington <Edington12@yahoo.com>
2023-03-27 21:00:48 -0400

    Set up for HM Proj.

---
+++ index.js
@@ -0,0 +1,7 @@
+import prompt from "readline-sync";
+import wordBank from "./word-bank.js";
+
+console.log("\nWelcome to Hangman!\nPress ctrl+c to stop\n");
+const letter = prompt.question("Please guess a letter: ");
+console.log(`You guessed "${letter}"`);
+
